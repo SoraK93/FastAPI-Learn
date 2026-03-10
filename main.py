5c44f38719cbd8da8073dc14d868894fefc3c4b6
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2026-03-10 11:23:14 +0530

    completed query param and string validation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from enum import Enum
-from fastapi import FastAPI, Query
-from pydantic import BaseModel, AfterValidator
+from fastapi import FastAPI, Path, Query
+from pydantic import AfterValidator, BaseModel
 from typing import Any, Annotated
 import random
 
@@ -18,7 +18,11 @@ class Item(BaseModel):
     tax: float | None = None
 
 
-app = FastAPI()
+app = FastAPI(
+    title="My First FastAPI - App",
+    version="0.2.0",
+    description="This is a project I am building to learn FastAPI"
+)
 
 fake_items_db = [{"item_name": "Foo"}, {
     "item_name": "Bar"}, {"item_name": "Baz"}]
@@ -37,18 +41,23 @@ def check_valid_id(id: str):
     return id
 
 
-@app.get("/items/")
+@app.get("/items/{item_id}", tags=["items"])
 async def read_items(
-    id: Annotated[str | None, AfterValidator(check_valid_id)] = None
-) -> dict[str, str | None]:
-    if id:
-        item = data.get(id)
+    item_id: Annotated[str, Path(title="The ID of the item to get"), AfterValidator(check_valid_id)],
+    q: Annotated[str | None, Query(alias="item-query")] = None
+) -> dict[str, Any]:
+    results: dict[str, int | str | None] = {"item_id": item_id}
+    if q:
+        results["q"] = q
+    if item_id:
+        results["data"] = data.get(item_id)
     else:
-        id, item = random.choice(list(data.items()))
-    return {"id": id, "name": item}
+        item_id, item = random.choice(list(data.items()))
+        results["data"] = item
+    return results
 
 
-@app.get("/items-q/")
+@app.get("/items-q/", tags=["items"])
 async def read_items_query(
     q: Annotated[
         str | None,
@@ -61,14 +70,14 @@ async def read_items_query(
             deprecated=True)] = None
 ):
     results: dict[str, list[dict[str, str]] | str] = {
-        "items": [{"item_id": "Foo"}, {"item_id": "Bar"}]
+        "items": fake_items_db
     }
     if q:
         results["q"] = q
     return results
 
 
-@app.post("/items/")
+@app.post("/items/", tags=["items"])
 async def create_item(item: Item):
     item_dict = item.model_dump()
     if item.tax is not None:
@@ -77,7 +86,7 @@ async def create_item(item: Item):
     return item
 
 
-@app.put("/items/{item_id}")
+@app.put("/items-update/{item_id}", tags=["items"])
 async def update_item(item_id: int, item: Item, q: str | None = None) -> dict[str, Any]:
     result: dict[str, Any] = {"item_id": item_id, **item.model_dump()}
     if q:
@@ -85,13 +94,13 @@ async def update_item(item_id: int, item: Item, q: str | None = None) -> dict[st
     return result
 
 
-@app.get("/items/{item_id}")
-async def read_user_item(item_id: str, needy: str, skip: int = 0, limit: int | None = None):
+@app.get("/items-get/{item_id}", tags=["items"])
+async def read_item(item_id: str, needy: str, skip: int = 0, limit: int | None = None):
     item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
     return item
 
 
-@app.get("/users/{user_id}/items/{item_id}")
+@app.get("/users/{user_id}/items/{item_id}", tags=["users"])
 async def read_user_item(user_id: int, item_id: str, q: str | None = None, short: bool = False):
     item = {"item_id": item_id, "owner_id": user_id}
     if q:
@@ -103,7 +112,7 @@ async def read_user_item(user_id: int, item_id: str, q: str | None = None, short
     return item
 
 
-@app.get("/models/{model_name}")
+@app.get("/models/{model_name}", tags=["models"])
 async def get_model(model_name: ModelName):
     if model_name is ModelName.alexnet:
         return {"model_name": model_name, "message": "Deep Learning FTW!"}
@@ -114,6 +123,6 @@ async def get_model(model_name: ModelName):
     return {"model_name": model_name, "message": "Have some rediduals"}
 
 
-@app.get("/files/{file_path:path}")
+@app.get("/files/{file_path:path}", tags=["files"])
 async def read_file(file_path: str):
     return {"file_path": file_path}
